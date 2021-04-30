d57ccb74441b44eb869a90fe229fd51d1f8b3233
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-04-30 11:47:57 +0900

    update by lint

--- src/oraclize.test.ts
+++ src/oraclize.test.ts
@@ -10,46 +10,46 @@ import * as capModules from './original/cap'
 import * as checkModules from './original/check'
 import { BaseProvider } from '@ethersproject/providers'
 
-
-
 let getProvider: sinon.SinonStub<[network: string], BaseProvider>
 let getCap: sinon.SinonStub<[provider: BaseProvider], Promise<BigNumber>>
-let isUpdateCap: sinon.SinonStub<[provider: BaseProvider, nextCap: BigNumber, transactionHash: string], Promise<boolean>>
+let isUpdateCap: sinon.SinonStub<
+	[provider: BaseProvider, nextCap: BigNumber, transactionHash: string],
+	Promise<boolean>
+>
 
 test.before(() => {
-	getProvider = sinon.stub(
-		providerModules,
-		'getProvider'
-	)
-	getProvider
-		.withArgs('mainnet')
-		.returns({ network: 'mainnet' } as any)
+	getProvider = sinon.stub(providerModules, 'getProvider')
+	getProvider.withArgs('mainnet').returns({ network: 'mainnet' } as any)
 
-	getCap = sinon.stub(
-		capModules,
-		'getCap'
-	)
-	getCap
-		.withArgs({ network: 'mainnet' } as any)
-		.resolves(BigNumber.from(100))
-	isUpdateCap = sinon.stub(
-		checkModules,
-		'isUpdateCap'
-	)
+	getCap = sinon.stub(capModules, 'getCap')
+	getCap.withArgs({ network: 'mainnet' } as any).resolves(BigNumber.from(100))
+	isUpdateCap = sinon.stub(checkModules, 'isUpdateCap')
 	isUpdateCap
-		.withArgs({ network: 'mainnet' } as any, BigNumber.from(100), 'dummy-transaction')
+		.withArgs(
+			{ network: 'mainnet' } as any,
+			BigNumber.from(100),
+			'dummy-transaction'
+		)
 		.resolves(true)
 	isUpdateCap
-		.withArgs({ network: 'mainnet' } as any, BigNumber.from(100), 'dummy-transaction2')
+		.withArgs(
+			{ network: 'mainnet' } as any,
+			BigNumber.from(100),
+			'dummy-transaction2'
+		)
 		.resolves(false)
 })
 
 test('Returns oraclize data', async (t) => {
 	const query = {
 		transactionhash: 'dummy-transaction',
-		publicSignature: 'dummy-sig'
+		publicSignature: 'dummy-sig',
 	}
-	const res = await oraclize({ query: query as any, network: 'mainnet', signatureOptions: null as any })
+	const res = await oraclize({
+		query: query as any,
+		network: 'mainnet',
+		signatureOptions: null as any,
+	})
 	t.is(res!.message, '100')
 	t.is(res!.status, 0)
 	t.is(res!.statusMessage, 'mainnet dummy-sig')
@@ -58,10 +58,14 @@ test('Returns oraclize data', async (t) => {
 test('Returns undefind', async (t) => {
 	const query = {
 		transactionhash: 'dummy-transaction2',
-		publicSignature: 'dummy-sig'
+		publicSignature: 'dummy-sig',
 	}
-	const res = await oraclize({ query: query as any, network: 'mainnet', signatureOptions: null as any })
-	t.true(typeof (res) === 'undefined')
+	const res = await oraclize({
+		query: query as any,
+		network: 'mainnet',
+		signatureOptions: null as any,
+	})
+	t.true(typeof res === 'undefined')
 })
 
 test.after(() => {
