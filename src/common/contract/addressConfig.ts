d94948545750879ef2f3cfec5d6a256a34bc5c84
aggre <hiroyuki.aggre@gmail.com>
2021-05-27 10:08:51 +0900

    Temporary hard coding

--- src/common/contract/addressConfig.ts
+++ src/common/contract/addressConfig.ts
@@ -1,5 +1,15 @@
 import { ethers, providers } from 'ethers'
-import { addresses } from '@devprotocol/dev-kit'
+
+const addresses = {
+	eth: {
+		main: {
+			registry: '0x1D415aa39D647834786EB9B5a333A50e9935b796',
+		},
+		ropsten: {
+			registry: '0xD6D07f1c048bDF2B3d5d9B6c25eD1FC5348D0A70',
+		},
+	},
+}
 
 export const getAddressConfigInstance = async (
 	provider: providers.BaseProvider
