05d076253a19564b3ca7748d1413e82448bd0537
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-06-21 10:02:22 +0900

    add log

--- src/addresses.ts
+++ src/addresses.ts
@@ -1,9 +1,16 @@
+/* eslint-disable functional/no-expression-statement */
 import { FunctionAddresses } from '@devprotocol/khaos-core'
 import { getLockupInstance } from './common'
 import { getProvider } from './common'
 
-export const addresses: FunctionAddresses = async ({ network }) => {
+export const addresses: FunctionAddresses = async ({ network, context }) => {
+	context?.log.info(`network name:${network}`)
+	const endpoint = process.env[`KHAOS_${network.toUpperCase()}_JSON_RPC`]
+	context?.log.info(`endpoint name:${endpoint}`)
 	const provider = getProvider(network)
+	const net = await provider.detectNetwork()
+	context?.log.info(`chainId id:${net.chainId}`)
 	const lockup = await getLockupInstance(provider)
+	context?.log.info(`lockup address:${lockup.address}`)
 	return lockup.address
 }
